dad72272b12c54b1a225635ae9c2a001c4f1d821
Brenna <bcmdarroch@gmail.com>
2017-02-27 10:53:11 -0800

    wrote tests for initialize and order_item, code passes all tests

--- lib/restaurant_bill.rb
+++ lib/restaurant_bill.rb
@@ -1,12 +1,12 @@
-# class RestaurantBill
-#   attr_reader :ordered_items
+class RestaurantBill
+  attr_reader :ordered_items
 
-#   def initialize
-#     @ordered_items = []
-#   end
+  def initialize
+    @ordered_items = []
+  end
 
-#   def order_item(item, cost)
-#     @ordered_items << [item, cost]
-#   end
+  def order_item(item, cost)
+    @ordered_items << [item, cost]
+  end
 
-# end
+end
--- specs/restaurant_bill_spec.rb
+++ specs/restaurant_bill_spec.rb
@@ -2,3 +2,42 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/restaurant_bill'
+
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new
+
+describe RestaurantBill do
+
+  before do
+    @my_bill = RestaurantBill.new
+  end
+
+  it "RestaurantBill is created with an empty ordered_items" do
+    @my_bill.ordered_items.must_be_instance_of Array
+    @my_bill.ordered_items.length.must_equal 0
+    @my_bill.ordered_items.empty?.must_equal true
+  end
+
+  it "order_item adds item and cost to bill" do
+    @my_bill.order_item("Pike Place Chowder", 8.5)
+    @my_bill.ordered_items.must_include ["Pike Place Chowder", 8.5]
+  end
+
+  it "order_item adds multiple orders into ordered_items" do
+    my_order = ["Pike Place Chowder", 8.5]
+    addie_order = ["New England Clam Chowder", 7.5]
+    rana_order = ["Salmon & Tomato Chowder", 10.5]
+
+    @my_bill.order_item(my_order[0], my_order[1])
+    @my_bill.order_item(addie_order[0], addie_order[1])
+    @my_bill.order_item(rana_order[0], rana_order[1])
+
+    @my_bill.ordered_items.must_include my_order
+    @my_bill.ordered_items.must_include addie_order
+    @my_bill.ordered_items.must_include rana_order
+  end
+
+  it "Calculate sub-total with tax" do
+
+  end
+end
